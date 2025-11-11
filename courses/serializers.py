524caf188b068e8693dfa30bac7970029f7ed0d6
NgigiN <ngigi.nyongo@gmail.com>
2025-11-11 22:31:52 +0300

    fix: update serializers to have read only fields and finalize on the transcript serializer

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import SemesterInfo, Course, Grade, ScheduleEntry, Transcript
+from users.models import StudentProfile
 
 class SemesterInfoSerializer(serializers.ModelSerializer):
   class Meta:
@@ -7,9 +8,10 @@ class SemesterInfoSerializer(serializers.ModelSerializer):
     fields = '__all__'
 
 class CourseSerializer(serializers.ModelSerializer):
-  semester_code = serializers.CharField(source='semester.code')
-  semester_name = serializers.CharField(source='semester.name')
+  semester_code = serializers.CharField(source='semester.code', read_only=True)
+  semester_name = serializers.CharField(source='semester.name', read_only=True)
   semester_id = serializers.IntegerField(write_only=True, required=False)
+
   class Meta:
     model = Course
     fields = '__all__'
@@ -29,10 +31,9 @@ class CourseSerializer(serializers.ModelSerializer):
 
 # TODO: Confirm if grades have to be linked to semesters or courses.
 class GradeSerializer(serializers.ModelSerializer):
-  semester_code = serializers.CharField(source='semester.code')
-  semester_name = serializers.CharField(source='semester.name')
+  semester_code = serializers.CharField(source='semester.code', read_only=True)
+  semester_name = serializers.CharField(source='semester.name', read_only=True)
   semester_id = serializers.IntegerField(write_only=True, required=False)
-  student_id = serializers.IntegerField(write_only=True, required=False)
 
   class Meta:
     model = Grade
@@ -52,9 +53,6 @@ class GradeSerializer(serializers.ModelSerializer):
     return super().update(instance, validated_data)
 
 class ScheduleEntrySerializer(serializers.ModelSerializer):
-  course_code = serializers.CharField(source='course.course_code')
-  course_name = serializers.CharField(source='course.course_name')
-  course_id = serializers.IntegerField(write_only=True, required=False)
   class Meta:
     model = ScheduleEntry
     fields = '__all__'
@@ -66,25 +64,26 @@ class ScheduleEntrySerializer(serializers.ModelSerializer):
         raise serializers.ValidationError("Start time must be before end time")
     return attrs
 
+class TranscriptSerializer(serializers.ModelSerializer):
+  student_id = serializers.CharField(source='student.student_id', read_only=True)
+  student_name = serializers.CharField(source='student.user.username', read_only=True)
+  program = serializers.CharField(source='student.program', read_only=True)
+  grades = GradeSerializer(many=True, read_only=True)
+  student_profile_id = serializers.IntegerField(write_only=True, required=False)
+
+  class Meta:
+    model = Transcript
+    fields = '__all__'
+    read_only_fields = ['created_at', 'updated_at']
+
   def create(self, validated_data):
-    course_id = validated_data.pop('course_id', None)
-    if course_id:
-      validated_data['course'] = Course.objects.get(id=course_id)
+    student_profile_id = validated_data.pop('student_profile_id', None)
+    if student_profile_id:
+      validated_data['student'] = StudentProfile.objects.get(id=student_profile_id)
     return super().create(validated_data)
 
   def update(self, instance, validated_data):
-    course_id = validated_data.pop('course_id', None)
-    if course_id:
-      validated_data['course'] = Course.objects.get(id=course_id)
+    student_profile_id = validated_data.pop('student_profile_id', None)
+    if student_profile_id:
+      validated_data['student'] = StudentProfile.objects.get(id=student_profile_id)
     return super().update(instance, validated_data)
-
-class TranscriptSerializer(serializers.ModelSerializer):
-   student_id = serializers.CharField(source='student.student_id', read_only=True)
-   student_name = serializers.CharField(source='student.user.username', read_only=True)
-   program = serializers.CharField(source='student.program', read_only=True)
-   grades = GradeSerializer(many=True, read_only=True, source='student.grades')
-
-   class Meta:
-      model = Transcript
-      fields = '__all__'
-      read_only_fields = ['created_at', 'updated_at']
