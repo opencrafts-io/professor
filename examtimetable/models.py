1051159ab272361eb576088b4eccf3c05d8b3595
NgigiN <ngigi.nyongo@gmail.com>
2025-11-18 18:51:49 +0300

    fix: Make ExamSchedule fields optional and create migrations

--- examtimetable/models.py
+++ examtimetable/models.py
@@ -5,12 +5,12 @@ from courses.models import SemesterInfo
 
 class ExamSchedule(models.Model):
     course_code = models.CharField(max_length=50)
-    course_name = models.CharField(max_length=255)
-    semester = models.ForeignKey(SemesterInfo, on_delete=models.CASCADE, related_name='exam_schedule')
+    course_name = models.CharField(max_length=255, null=True, blank=True)
+    semester = models.ForeignKey(SemesterInfo, on_delete=models.CASCADE, related_name='exam_schedule', null=True, blank=True)
 
-    exam_date = models.DateField()
-    start_time = models.TimeField()
-    end_time = models.TimeField()
+    exam_date = models.DateField(null=True, blank=True)
+    start_time = models.TimeField(null=True, blank=True)
+    end_time = models.TimeField(null=True, blank=True)
 
 # Optional fields to support custom formats of timetables.
     day = models.CharField(max_length=50, null=True, blank=True)
@@ -32,5 +32,9 @@ class ExamSchedule(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.course_code} - {self.exam_date} {self.start_time}"
+        if self.exam_date and self.start_time:
+            return f"{self.course_code} - {self.exam_date} {self.start_time}"
+        elif self.day:
+            return f"{self.course_code} - {self.day}"
+        return f"{self.course_code}"
 
