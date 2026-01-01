732f9d8bed1e92884f88216a8addc0a8c20b3ffe
Erick <hearteric57@gmail.com>
2026-01-01 11:02:05 +0300

    chore: adds a  model to represent magnet scrapping configuration commands

--- magnet/models.py
+++ magnet/models.py
@@ -1,3 +1,33 @@
+import uuid
 from django.db import models
 
+
 # Create your models here.
+class MagnetConfig(models.Model):
+    """
+    A model representing a magnet command composed of instructions
+    """
+
+    command_id = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        primary_key=True,
+    )
+
+    name = models.CharField(
+        max_length=60,
+        null=False,
+    )
+
+    description = models.TextField(default="")
+
+    requires_interaction = models.BooleanField(
+        default=False,
+    )
+
+    created_at = models.DateField(auto_now_add=True)
+
+    instructions = models.JSONField(default=list)
+
+    def __str__(self) -> str:
+        return f"{self.name} - {self.description[:20]}"
