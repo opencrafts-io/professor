7d9aec42f81161bffa07e2aac6efffb819ae2ea1
Erick <hearteric57@gmail.com>
2026-01-01 11:19:37 +0300

    chore: adds an  model to represent institutions information

--- institutions/models.py
+++ institutions/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
 
 # Create your models here.
+class Institution(models.Model):
+    """
+    An institution model represents a real life institution e.g Stanford
+    """
+
+    institution_id = models.BigAutoField(
+        primary_key=True,
+    )
+    name = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False,
+    )
+    web_pages = ArrayField(
+        models.URLField(),
+    )
+    domains = ArrayField(models.CharField(max_length=255))
+    country = models.CharField(max_length=100)
+    state_province = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.institution_id} - {self.name}"
