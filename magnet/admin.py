a33deb4857729c9dfc61833397d390c19c20e8f0
Erick <hearteric57@gmail.com>
2026-01-02 10:48:56 +0300

    chore: registers magnet scrapping command model to the admin interface for management

--- magnet/admin.py
+++ magnet/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import MagnetScrappingCommand
 
-# Register your models here.
+
+class MagnetScrappingCommandAdmin(admin.ModelAdmin):
+    list_display = [
+        "institution",
+        "command_id",
+        "name",
+        "description",
+        "requires_interaction",
+        "created_at",
+    ]
+
+    list_filter = [
+        "institution",
+        "command_id",
+        "name",
+        "requires_interaction",
+        "created_at",
+    ]
+
+
+admin.site.register(
+    MagnetScrappingCommand,
+    MagnetScrappingCommandAdmin,
+)
