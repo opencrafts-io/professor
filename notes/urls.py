df8fabdbe0e7c05d19e1415fc67dc31e54c55c32
NgigiN <ngigi.nyongo@gmail.com>
2026-08-31 16:32:06 +0300

    feat: scaffold notes app mounted at api/notes/

---
+++ notes/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+from . import views  # noqa: F401  (views wired in later tasks)
+
+urlpatterns = []
--- professor/settings.py
+++ professor/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     "examtimetable",
     "users",
     "magnet",
+    "notes",
 ]
 
 
